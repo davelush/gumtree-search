58d6086f4f03804d770f13aef64a9e3b92c03c63
davelush <davelush@github.com>
2020-09-14 21:22:35 +0100

    Sanity check structure and a bookmark to current location

--- src/cheeriocrawler-tutorial-2.js
+++ src/cheeriocrawler-tutorial-2.js
@@ -1,5 +1,26 @@
 const Apify = require('apify');
 
-Apify.main(async() => {
+Apify.main(async () => {
+    const sources = [
+        'https://apify.com/store?category=TRAVEL',
+        'https://apify.com/store?category=ECOMMERCE',
+        'https://apify.com/store?category=ENTERTAINMENT',
+    ];
 
+    const requestList = await Apify.openRequestList('categories', sources);
+
+    const crawler = new Apify.CheerioCrawler({
+        requestList,
+        handlePageFunction: async ({ $, request }) => {
+            // Select all the actor cards.
+            $('.item').each((i, el) => {
+                const text = $(el).text();
+                console.log(`ITEM: ${text}\n`);
+            });
+        },
+    });
+
+    // Just got to https://sdk.apify.com/docs/guides/getting-started#enqueueing-the-detail-links-using-a-custom-selector
+
+    await crawler.run();
 });
