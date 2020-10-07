4c5291084521a6035cfd4355c050044ef0e940bd
davelush <dave.lush@gmail.com>
2020-10-07 20:54:17 +0100

    parsing out results data from apify

--- src/cheeriocrawler-tutorial-2.js
+++ src/cheeriocrawler-tutorial-2.js
@@ -17,8 +17,33 @@ Apify.main(async () => {
         maxRequestsPerCrawl: 50,
         requestList,
         requestQueue,
-        handlePageFunction: async ({ $, request }) => {
+        handlePageFunction: async ({$, request}) => {
             console.log(`Processing ${request.url}`);
+
+            if (request.userData.detailPage) {
+                const urlArr = request.url.split('/').slice(-2);
+
+                const results = {
+                    url: request.url,
+                    uniqueIdentifier: urlArr.join('/'),
+                    owner: urlArr[0],
+                    title: $('header h1').text(),
+                    description: $('header p[class^=Text__Paragraph]').text(),
+                    lastRunDate: new Date(
+                        Number($('time')
+                            .eq(1)
+                            .attr('datetime'),
+                        ),
+                    ),
+                    runCount: Number(
+                        $('ul.stats li:nth-of-type(3)')
+                            .text()
+                            .match(/\d+/)[0],
+                    )
+                };
+
+                console.log('RESULTS', results);
+            }
 
             // Only enqueue new links from the category pages.
             if (!request.userData.detailPage) {
