0e87c1d703d6e117871cefbbdbd547fac71de227
davelush <dave.lush@gmail.com>
2020-10-14 20:48:45 +0100

    Added a prepareRequestFunction to ensure we always set a user-agent

--- src/main.js
+++ src/main.js
@@ -21,6 +21,12 @@ Apify.main(async () => {
             log.info(`Processing ${request.url}`);
             await router(request.userData.label, context);
         },
+        prepareRequestFunction: async context => {
+            context.request.headers = {
+                "User-Agent" : `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Mobile Safari/537.36`
+            };
+            console.log(context.request.headers);
+        },
     });
 
     log.info('Starting the crawl.');
