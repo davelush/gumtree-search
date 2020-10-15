907365eca6e1efa10d9f5a92429e4fa938718d57
davelush <dave.lush@gmail.com>
2020-10-15 21:56:13 +0100

    Introduced a session pool of size 1 and usage of external getUserAgent()

--- src/main.js
+++ src/main.js
@@ -16,6 +16,11 @@ Apify.main(async () => {
     const crawler = new Apify.CheerioCrawler({
         requestList,
         requestQueue,
+        useSessionPool: true,
+        sessionPoolOptions: {
+            maxPoolSize: 1
+        },
+        persistCookiesPerSession: true,
         handlePageFunction: async context => {
             const { request } = context;
             log.info(`Processing ${request.url}`);
@@ -23,15 +28,8 @@ Apify.main(async () => {
         },
         prepareRequestFunction: async context => {
             context.request.headers = {
-                "user-agent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Mobile Safari/537.36",
-                "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "accept-encoding" : "gzip, deflate, br",
-                "accept-language" : "en-GB,en-US;q=0.9,en;q=0.8,es;q=0.7",
                 "cache-control" : "max-age=0",
-                "authority" : "www.gumtree.com",
-                "dnt" : "1",
-                "referer" : "https://www.gumtree.com/search?search_category=for-sale&search_location=wf120rg&distance=10",
-                "upgrade-insecure-requests": "1"
+                "user-agent" : tools.getUserAgent(),
             };
         },
     });
